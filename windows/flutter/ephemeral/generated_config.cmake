449fd51151d11f23941df1e0336d0c521b84f08d
roya-zadran <sirinamiri@springpax.org>
2024-10-28 21:50:36 +0430

    Add files via upload

---
+++ windows/flutter/ephemeral/generated_config.cmake
@@ -0,0 +1,24 @@
+# Generated code do not commit.
+file(TO_CMAKE_PATH "C:\\Users\\AhmadShahZadran\\AppData\\Local\\Flutter SDK" FLUTTER_ROOT)
+file(TO_CMAKE_PATH "D:\\Flutter Projects\\destiny_starting_project" PROJECT_DIR)
+
+set(FLUTTER_VERSION "1.0.0+1" PARENT_SCOPE)
+set(FLUTTER_VERSION_MAJOR 1 PARENT_SCOPE)
+set(FLUTTER_VERSION_MINOR 0 PARENT_SCOPE)
+set(FLUTTER_VERSION_PATCH 0 PARENT_SCOPE)
+set(FLUTTER_VERSION_BUILD 1 PARENT_SCOPE)
+
+# Environment variables to pass to tool_backend.sh
+list(APPEND FLUTTER_TOOL_ENVIRONMENT
+  "FLUTTER_ROOT=C:\\Users\\AhmadShahZadran\\AppData\\Local\\Flutter SDK"
+  "PROJECT_DIR=D:\\Flutter Projects\\destiny_starting_project"
+  "FLUTTER_ROOT=C:\\Users\\AhmadShahZadran\\AppData\\Local\\Flutter SDK"
+  "FLUTTER_EPHEMERAL_DIR=D:\\Flutter Projects\\destiny_starting_project\\windows\\flutter\\ephemeral"
+  "PROJECT_DIR=D:\\Flutter Projects\\destiny_starting_project"
+  "FLUTTER_TARGET=D:\\Flutter Projects\\destiny_starting_project\\lib\\main.dart"
+  "DART_DEFINES=Zmx1dHRlci5pbnNwZWN0b3Iuc3RydWN0dXJlZEVycm9ycz10cnVl"
+  "DART_OBFUSCATION=false"
+  "TRACK_WIDGET_CREATION=true"
+  "TREE_SHAKE_ICONS=false"
+  "PACKAGE_CONFIG=D:\\Flutter Projects\\destiny_starting_project\\.dart_tool\\package_config.json"
+)
